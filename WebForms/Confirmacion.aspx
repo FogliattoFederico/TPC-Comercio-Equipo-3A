0bd3a5cde0d1ee717b5c3b00f850c51192915d0b
Federico Fogliatto <fede.fogliatto@gmail.com>
2025-06-24 23:32:29 -0300

    funcion de recuperar cuenta (no resuelto)

---
+++ WebForms/Confirmacion.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Confirmacion.aspx.cs" Inherits="WebForms.Confirmacion" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="css/StyleError.css" rel="stylesheet" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        
+     <div class="error-container text-center">
+     
+     <div class="error-icon">
+         <i class="bi bi-exclamation-triangle-fill"></i>
+     </div>
+
+     
+     <h3 class="mb-3">En breve recibiras un email con la informacion solicitada</h3>
+
+    
+     <asp:Label ID="lblMensaje" runat="server" CssClass="fs-5 text-muted mb-4 d-block" Text=""></asp:Label>
+
+     
+     <a href="Default.aspx" class="btn-home">
+         <i class="bi bi-house-door me-2"></i>Volver al inicio
+     </a>
+ </div>
+        
+</asp:Content>
--- WebForms/Recuperar.aspx.cs
+++ WebForms/Recuperar.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dominio;
+using Negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,12 +13,61 @@ namespace WebForms
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            lblMensaje.Text = "";
         }
 
         protected void btnRecuperar_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (ValidarCampos())
+                {
+                    UsuarioNegocio negocio = new UsuarioNegocio();
+                    EmailService email = new EmailService();
 
+                    List<Usuario> lista = negocio.Listar();
+
+                    Usuario encontrado = lista.FirstOrDefault(x => x.Email == txtEmail.Text);
+
+                    if (encontrado == null)
+                    {
+                        lblMensaje.Text = "Email no encontrado";
+                        return;
+                    }
+
+                    lblMensaje.Text = "";
+
+                    string asunto = "Recuperar datos de tu cuenta";
+                    string cuerpo = "Usuario : " + encontrado.NombreUsuario + " Contraesña : " + encontrado.Contrasena;
+
+                    email.ArmarCorreo(txtEmail.Text, asunto, cuerpo);
+                    email.enviarEmail();
+
+                    Response.Redirect("Confirmacion.aspx", false);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Session.Add("Error.aspx", ex.ToString());
+            }
+            
+        }
+
+        private bool ValidarCampos()
+        {
+            lblMensaje.ForeColor = System.Drawing.Color.Red;
+
+            
+            string email = txtEmail.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                lblMensaje.Text = "Todos los campos deben estar completos.";
+                return false;
+            }
+
+            return true;
         }
     }
 }
